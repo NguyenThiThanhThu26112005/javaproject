e8b898c3c5aa3e08bbddcb79e3655825c2c958c9
NguyenThiThanhThu26112005 <thu239734@gmail.com>
2025-07-21 19:40:08 +0700

    first

---
+++ src/main/HelloWorld.java
@@ -0,0 +1,13 @@
+package main;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world!! Toi la Thanh Thu");
+		System.out.println("Hello world!! Toi la Thanh Thu");
+		System.out.println("Hello world!! Toi la Thanh Thu");
+		System.out.println("Hello world!! Toi la Thanh Thu");
+		System.out.println("Hello world!! Toi la Thanh Thu");
+
+	}
+
+}
